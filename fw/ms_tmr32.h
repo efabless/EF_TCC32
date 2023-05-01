330c32faa774ebc32d170055a38451d50ff3e424
Mohamed Shalan <mshalan@aucegypt.edu>
2023-05-01 10:09:35 +0200

    Adding C header file

--- fw/ms_tmr32.h
+++ fw/ms_tmr32.h
@@ -3,35 +3,46 @@
 
 */
 
-#define MS_TMR32_BASE   0x10000000
+#define MS_TMR32_BASE                   0x10000000
 
-#define TMR_REG_ADDR        (MS_TMR32_BASE +  0x00)
-#define PERIOD_REG_ADDR     (MS_TMR32_BASE +  0x04)
-#define PWMCMP_REG_ADDR     (MS_TMR32_BASE +  0x08)
-#define MATCH_REG_ADDR      (MS_TMR32_BASE +  0x0C)
-#define COUNTER_REG_ADDR    (MS_TMR32_BASE +  0x10,)
-#define CTRL_REG_ADDR       (MS_TMR32_BASE +  0x100)
-#define RIS_REG_ADDR        (MS_TMR32_BASE +  0x200)
-#define MIS_REG_ADDR        (MS_TMR32_BASE +  0x204)
-#define IM_REG_ADDR         (MS_TMR32_BASE +  0x208)
-#define ICR_REG_ADDR        (MS_TMR32_BASE +  0x20C)
-    
-#define CTRL_EN             0x1
-#define CTRL_TMR_EN         0x2
-#define CTRL_PWM_EN         0x4
-#define CTRL_COUNTER_EN     0x8
-#define CTRL_CLK_SRC_CLK2   0x000
-#define CTRL_CLK_SRC_CLK4   0x100
-#define CTRL_CLK_SRC_CLK8   0x200
-#define CTRL_CLK_SRC_CLK16  0x300
-#define CTRL_CLK_SRC_CLK32  0x400
-#define CTRL_CLK_SRC_CLK64  0x500
-#define CTRL_CLK_SRC_CLK128 0x600
-#define CTRL_CLK_SRC_CLK256 0x700
-#define CTRL_CLK_SRC_CLK    0x800
-#define CTRL_CLK_SRC_EXTCTR 0x900
-#define CTRL_UP             0x10000
-#define CTRL_ONE_SHOT       0x20000
-#define CTRL_EVENT_POS      0x1000000
-#define CTRL_EVENT_NEG      0x2000000
-#define CTRL_EVENT_BOTH     0x3000000
+#define MS_TMR32_TMR_REG_ADDR           (MS_TMR32_BASE +  0x00)
+#define MS_TMR32_PERIOD_REG_ADDR        (MS_TMR32_BASE +  0x04)
+#define MS_TMR32_PWMCMP_REG_ADDR        (MS_TMR32_BASE +  0x08)
+#define MS_TMR32_MATCH_REG_ADDR         (MS_TMR32_BASE +  0x0C)
+#define MS_TMR32_COUNTER_REG_ADDR       (MS_TMR32_BASE +  0x10,)
+#define MS_TMR32_CTRL_REG_ADDR          (MS_TMR32_BASE +  0x100)
+#define MS_TMR32_RIS_REG_ADDR           (MS_TMR32_BASE +  0x200)
+#define MS_TMR32_MIS_REG_ADDR           (MS_TMR32_BASE +  0x204)
+#define MS_TMR32_IM_REG_ADDR            (MS_TMR32_BASE +  0x208)
+#define MS_TMR32_ICR_REG_ADDR           (MS_TMR32_BASE +  0x20C)
+
+#define MS_TMR32_CTRL_EN                0x1
+#define MS_TMR32_CTRL_TMR_EN            0x2
+#define MS_TMR32_CTRL_PWM_EN            0x4
+#define MS_TMR32_CTRL_COUNTER_EN        0x8
+#define MS_TMR32_CTRL_CLK_SRC_CLK2      0x000
+#define MS_TMR32_CTRL_CLK_SRC_CLK4      0x100
+#define MS_TMR32_CTRL_CLK_SRC_CLK8      0x200
+#define MS_TMR32_CTRL_CLK_SRC_CLK16     0x300
+#define MS_TMR32_CTRL_CLK_SRC_CLK32     0x400
+#define MS_TMR32_CTRL_CLK_SRC_CLK64     0x500
+#define MS_TMR32_CTRL_CLK_SRC_CLK128    0x600
+#define MS_TMR32_CTRL_CLK_SRC_CLK256    0x700
+#define MS_TMR32_CTRL_CLK_SRC_CLK       0x800
+#define MS_TMR32_CTRL_CLK_SRC_EXTCTR    0x900
+#define MS_TMR32_CTRL_UP                0x10000
+#define MS_TMR32_CTRL_ONE_SHOT          0x20000
+#define MS_TMR32_CTRL_EVENT_POS         0x1000000
+#define MS_TMR32_CTRL_EVENT_NEG         0x2000000
+#define MS_TMR32_CTRL_EVENT_BOTH        0x3000000
+
+volatile unsigned int * ms_tmr32_tmr_reg        = (volatile unsigned int *) (MS_TMR32_TMR_REG_ADDR    );
+volatile unsigned int * ms_tmr32_period_reg     = (volatile unsigned int *) (MS_TMR32_PERIOD_REG_ADDR );
+volatile unsigned int * ms_tmr32_pwmcmp_reg     = (volatile unsigned int *) (MS_TMR32_PWMCMP_REG_ADDR );
+volatile unsigned int * ms_tmr32_match_reg      = (volatile unsigned int *) (MS_TMR32_MATCH_REG_ADDR  );
+volatile unsigned int * ms_tmr32_counter_reg    = (volatile unsigned int *) (MS_TMR32_COUNTER_REG_ADDR);
+volatile unsigned int * ms_tmr32_ctrl_reg       = (volatile unsigned int *) (MS_TMR32_CTRL_REG_ADDR   );
+volatile unsigned int * ms_tmr32_ris_reg        = (volatile unsigned int *) (MS_TMR32_RIS_REG_ADDR    );
+volatile unsigned int * ms_tmr32_mis_reg        = (volatile unsigned int *) (MS_TMR32_MIS_REG_ADDR    );
+volatile unsigned int * ms_tmr32_im_reg         = (volatile unsigned int *) (MS_TMR32_IM_REG_ADDR     );
+volatile unsigned int * ms_tmr32_icr_reg        = (volatile unsigned int *) (MS_TMR32_ICR_REG_ADDR    );
